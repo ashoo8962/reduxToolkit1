85e7eeb1ad6bfbcfa30ff302e446a5491439dfda
as <you@example.com>
2024-10-19 16:46:16 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { decrement, increment,reset ,incrementByAmount} from './features/counter/counterSlice'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount,setAmount]=useState(0)
+
+  const count=useSelector((state)=>state.counter.value)
+  const dispatch=useDispatch()
+  
+  function handleIncrement(){
+dispatch(increment())
+  }
+
+  function handleDecrement()
+  {
+dispatch(decrement())
+  }
+  function handleReset()
+  {
+    dispatch(reset())
+  }
+
+  function handleIncAmntClk()
+  {
+    dispatch(incrementByAmount(amount))
+  }
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='container'>
+         
+<button onClick={handleIncrement}>+</button>
+<p>Count:{count}</p>
+<button onClick={handleDecrement}>-</button>
+        </div><br />
+        <div>
+        <button onClick={handleReset}>Reset</button>
+
+        </div>
+        <input
+         type="number"
+         value={amount
+               } 
+         placeholder='enter val'
+         onChange={(e)=>setAmount(e.target.value)}
+     
+         />
+         <button onClick={handleIncAmntClk}> Inc my amount</button>
     </>
   )
 }
---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from '../features/counter/counterSlice.jsx'
+export const store=configureStore({
+    reducer:{
+        counter:counterReducer
+    },
+})
